2a4c01d43308ce3b659d4eaadfa7637cf3bfdad3
Notnox <llrricardo@gmail.com>
2021-02-08 22:35:30 -0300

    [SP05] 5 - Filtro de Alunos

--- app/src/components/Corpo/Lista/Lista.js
+++ app/src/components/Corpo/Lista/Lista.js
@@ -2,16 +2,25 @@ import React from 'react';
 
 const Lista = () => {
     return (
-        <table>
-            <tr>
-                <th>Nome</th>
-                <th>Data de nascimento</th>
-                <th>Turma</th>
-                <th>Telefone para Emergências</th>
-                <th>Em caso de emergência avisar</th>
-                <th colSpan="2">Ferramentas</th>
-            </tr>
-        </table>
+        <>
+            <header>
+                <label>
+                    <h3>Informe o nome do aluno</h3>
+                    <input type='text' placeholder="Digite aqui o nome." />
+                </label>
+                <button>Pesquisar</button>
+            </header>
+            <table>
+                <tr>
+                    <th>Nome</th>
+                    <th>Data de nascimento</th>
+                    <th>Turma</th>
+                    <th>Telefone para Emergências</th>
+                    <th>Em caso de emergência avisar</th>
+                    <th colSpan="2">Ferramentas</th>
+                </tr>
+            </table>
+        </>
     );
 };
 
