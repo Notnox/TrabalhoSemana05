258a4a3ff669adeec04e4ac03554919dd4bb3d7d
Notnox <llrricardo@gmail.com>
2021-02-08 22:30:58 -0300

    [SP05] 4 - Listagem de Alunos

--- app/src/components/Corpo/Lista/Lista.js
+++ app/src/components/Corpo/Lista/Lista.js
@@ -9,6 +9,7 @@ const Lista = () => {
                 <th>Turma</th>
                 <th>Telefone para Emergências</th>
                 <th>Em caso de emergência avisar</th>
+                <th colSpan="2">Ferramentas</th>
             </tr>
         </table>
     );
