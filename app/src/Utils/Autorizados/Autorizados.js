16b601c5c9434e87d195bc9914d9768e17ea322b
Notnox <74467549+Notnox@users.noreply.github.com>
2021-02-15 11:39:46 -0300

    Autorizados
    [SP05] 8 - Componente de Pessoas Autorizadas
    [SP05] 15 - Lista de Pessoas Autorizadas

---
+++ app/src/Utils/Autorizados/Autorizados.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Autorizados = ({ Autorizados }) => {
+    return (
+        false
+    );
+};
+
+export default Autorizados;
--- app/src/components/Form/Form.js
+++ app/src/components/Form/Form.js
@@ -2,7 +2,36 @@ import React, { useEffect, useState } from 'react';
 import Data from '../../Utils/Data/Data';
 import Telefone from '../../Utils/Telefone/Telefone';
 
-const Form = () => {
+const INITIAL_CADASTRO = {
+    autorizados: {}
+};
+
+const INITIAL_AUTORIZADOS = {
+    nome: '',
+    parentesco: ''
+};
+
+const Form = ({ }) => {
+
+    const [valueCadastro, setValueCadastro] = useState(INITIAL_CADASTRO);
+    const [autorizado, setAutorizado] = useState(INITIAL_AUTORIZADOS);
+    const [autorizados, setAutorizados] = useState([]);
+
+    const onChangeAutorizado = (e) => {
+        const { name, value } = e.target;
+
+        setAutorizado({ ...autorizado, [name]: value });
+
+    }
+
+    const adicionarAutorizado = () => {
+        if (autorizado.nome.length === 0) { return alert('Informe um autorizado') };
+        if (autorizado.parentesco.length === 0) { return alert('Informe o parentesco') };
+
+        setAutorizados([...autorizados, autorizado]);
+        setAutorizado(INITIAL_AUTORIZADOS);
+    }
+
     return (
         <form>
             <label>
@@ -10,9 +39,32 @@ const Form = () => {
                 <Telefone />
             </label>
             <label>
-                <p>Número do telefone</p>
+                <p>Data</p>
                 <Data desc="Informe a data" />
             </label>
+            <div>
+                <p>Autorizados para retirada</p>
+                {autorizados.length > 0 && autorizados.map((e) => {
+                    {
+                        return <section><span>{e.nome}</span><span>{e.parentesco}</span></section>
+                    }
+                })}
+                <label>
+                    <p>Nome</p>
+                    <input type='text' placeholder='Informe o nome' name='nome' onChange={onChangeAutorizado} value={autorizado.nome}/>
+                </label>
+                <label>
+                    <p>Parentesco</p>
+                    <select name='parentesco' onChange={onChangeAutorizado} value={autorizado.parentesco}>
+                        <option value='Selecione'>Selecione</option>
+                        <option value='Pais'>Pais</option>
+                        <option value='Irmão/ã'>Irmão/ã</option>
+                        <option value='Tios'>Tios</option>
+                        <option value='Avós'>Avós</option>
+                    </select>
+                </label>
+                <button type='button' onClick={adicionarAutorizado}>+</button>
+            </div>
         </form>
     );
 };
